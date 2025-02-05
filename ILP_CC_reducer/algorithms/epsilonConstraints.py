43f5efd982a89cf26670f02e4475f0e8439b1771
anhe2p9 <adriana_nh@uma.es>
2025-02-05 19:51:09 +0100

    Trying to implement e-constraint algorithm

--- ILP_CC_reducer/algorithms/epsilonConstraints.py
+++ ILP_CC_reducer/algorithms/epsilonConstraints.py
@@ -44,12 +44,11 @@ class EpsilonConstraintAlgorithm(Algorithm):
         #
         #
 
-        
+        # Solve {min f2}
         if hasattr(model, 'obj'):
             model.del_component('obj')
         model.add_component('obj', pyo.Objective(rule=lambda m: multiobj_model.LOCdifferenceObjective(m)))
-         
-        
+
         concrete = model.create_instance(data)
         solver=pyo.SolverFactory('cplex')
         results = solver.solve(concrete)
@@ -57,35 +56,81 @@ class EpsilonConstraintAlgorithm(Algorithm):
         
         
         if results.solver.status == 'ok':
+            
+            """ Solve {min f1(x) subject to f2(x) <= f2(z)} """
+            # f2(z) = f2
             f2 = pyo.value(multiobj_model.LOCdifferenceObjective(concrete))
             
+            # new static variable to implement new constraint f2(x) <= f2(z)
             if hasattr(model, 'f2'):
                 model.del_component('f2')
             model.add_component('f2', pyo.Var(initialize=f2))
             
+            # new constraint f2(x) <= f2(z)
             if hasattr(model, 'f2Constraint'):
                 model.del_component('f2Constraint')
-            model.add_component('f2Constraint', pyo.Constraint(rule=lambda m: multiobj_model.LOCdifferenceObjective(m)))
+            model.add_component('f2Constraint', pyo.Constraint(rule=lambda m: multiobj_model.LOCdifferenceConstraint(m)))
             
+            # new objective min f1(x)
             if hasattr(model, 'obj'):
                 model.del_component('obj')
             model.add_component('obj', pyo.Objective(rule=lambda m: multiobj_model.sequencesObjective(m)))
             
             solver=pyo.SolverFactory('cplex')
             results = solver.solve(concrete)
+                        
             
-            print(results)
+            """ z <- solve {min f1(x) subject to f2(x) <= f2(z)} """
+            # f1(z)
+            fp1 = pyo.value(multiobj_model.sequencesObjective(concrete))
+            # add f1(z) to pareto front
+            pareto_front = [fp1]
+            
+            # epsilon <- f1(z) - 1
+            epsilon = fp1 - 1
+            
+            # Delete f2Constraint to obtain the lower bound of f1(x) (first component of a utopian point)
+            if hasattr(model, 'f2Constraint'):
+                model.del_component('f2Constraint')
+            solver=pyo.SolverFactory('cplex')
+            results = solver.solve(concrete)  
+            
+            # min f1(x)
+            f1 = pyo.value(multiobj_model.sequencesObjective(concrete))
+            # lower bound for f1(x)
+            u1 = -10000 # NO SÉ CÓMO HACER QUE FUNCIONE SIN QUE SEA NEGATIVO
+            
             
             
             print(f"f2: {f2}")
-            f1 = pyo.value(multiobj_model.sequencesObjective(concrete))
-            pareto_front = []
+            print(f"fp1: {fp1}")
+            print(f"epsilon: {epsilon}")
+            print(f"f1: {f1}")
+            print(f"u1: {u1}")
             
-            print(f"fp1: {f1}")
-            epsilon = f1 - 1
+            f1_x = lambda m: multiobj_model.sequencesObjective(m)
+            
+            print(f1_x(concrete))
             
             
-            # while results.solver.status == 'ok' and len_sequences > fp1[0]:
+            while results.solver.status == 'ok' and f1 <= epsilon and False: # NO SÉ CÓMO PONER f1(x), ESO ES f1(z)
+                lambd = 1/(fp1 - u1)
+                
+                
+                
+                
+                f1 = pyo.value(multiobj_model.sequencesObjective(concrete))
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
                 # solution = solution +1
                 # concrete.epsilon[1]=f1-1
                 # results = solver.solve(concrete)

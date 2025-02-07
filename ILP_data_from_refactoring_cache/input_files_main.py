4cead989609b21deb91052c1d6bf78135ebf2fdd
anhe2p9 <adriana_nh@uma.es>
2025-02-07 10:19:00 +0100

    Minor changes

--- ILP_data_from_refactoring_cache/input_files_main.py
+++ ILP_data_from_refactoring_cache/input_files_main.py
@@ -2,21 +2,24 @@
 from ILP_data_from_refactoring_cache.utils import dataset as dataset, refactoring_cache as rc
 import argparse
 
+import os
+from pathlib import Path
+
 
 # Main function
-def main(path_to_refactoring_cache: str, output_folder: str):
+def main(path_to_refactoring_cache: str, output_folder: str, files_n: str):
     df = rc.set_extractions_id(dataset.dataframe_from_csv_file(path_to_refactoring_cache))
 
     # Save the extractions in conflict into a CSV file
-    dataset.dataframe_into_csv_file(rc.get_conflicts(df), output_folder + "_conflict.csv")
+    dataset.dataframe_into_csv_file(rc.get_conflicts(df), output_folder + f"/{files_n}_conflict.csv")
 
     # Save the nested extractions into a CSV file
     dataset.dataframe_into_csv_file(rc.get_nested_extraction_for_each_extraction(df, ["reductionCC"]),
-                                    output_folder + "_nested.csv")
+                                    output_folder + f"/{files_n}_nested.csv")
 
     # Save the lines of code and cognitive complexity of the extractions into a CSV file   
     dataset.dataframe_into_csv_file(rc.get_nested_extraction_for_each_extraction_computing_ccr(df),
-                                    output_folder + "_sequences.csv")
+                                    output_folder + f"/{files_n}_sequences.csv")
 
 
 # Call the main function
@@ -25,5 +28,16 @@ if __name__ == "__main__":
     parser.add_argument('input_file', type=str, help='Path to the refactoring cache CSV file')
     parser.add_argument('output_folder', type=str, help='Folder to save the output CSV files')
     args = parser.parse_args()
-
-    main(args.input_file, args.output_folder)
+    
+    
+    nested_directory_path = Path("original_code_data/bytecode-viewer")
+    
+    # Create nested directories
+    nested_directory_path.mkdir(parents=True, exist_ok=True)
+    
+    directory_path_str = str(nested_directory_path)
+    
+    last_dir = os.path.basename(nested_directory_path)
+    
+    
+    main(args.input_file, directory_path_str, last_dir)

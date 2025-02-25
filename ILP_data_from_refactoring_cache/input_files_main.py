ee073ad8e075976f70f493f28f7cdf109aa0f6c5
Adriana Novoa <anhe2p9@uma.es>
2025-02-25 18:26:06 +0100

    Minor changes

--- ILP_data_from_refactoring_cache/input_files_main.py
+++ ILP_data_from_refactoring_cache/input_files_main.py
@@ -1,5 +1,5 @@
 # Import the required libraries
-from ILP_data_from_refactoring_cache.utils import dataset as dataset, refactoring_cache as rc
+from utils import dataset as dataset, refactoring_cache as rc
 import argparse
 
 from pathlib import Path
